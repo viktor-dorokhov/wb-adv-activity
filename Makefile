9f48da4d1c03d02008df44a9789e095b2688083c
Viktor Dorokhov <superdva@yandex.ru>
2024-09-03 23:24:19 +0300

    first version

---
+++ Makefile
@@ -0,0 +1,17 @@
+install:
+	npm install
+
+dev:
+	npm run dev
+
+start:
+	npm run start
+
+lint:
+	npx eslint .
+
+test:
+	npm test -s
+
+test-coverage:
+	npm test -- --coverage
---
+++ index.js
@@ -0,0 +1,47 @@
+import 'dotenv/config';
+import axios from 'axios';
+import { format as formatDate, parse as parseDate, differenceInCalendarDays } from 'date-fns';
+
+const WB_API_KEY_ADV = process.env.WB_API_KEY_ADV;
+const apiUrl = 'https://advert-api.wildberries.ru/adv/v0/';
+
+// const campaignIds = process.env.CAMPAIGN_IDS.split(',');
+// пока только одна кампания
+const campaignId = process.env.CAMPAIGN_IDS;
+// пока только одно время и только старт
+let timeStart = process.env.TIME_START;
+// const timePause = process.env.TIME_PAUSE;
+let globalTimer;
+let started = false;
+
+const campaignAction = async (id, action) => {
+  const now = new Date(); 
+  try {
+    const response = await axios({
+      method: 'get',
+      url: `${apiUrl}${action}?id=${id}`,
+      responseType: 'json',
+      headers: { 'Authorization': WB_API_KEY_ADV },
+    });
+    started = true;
+    console.log(`[${formatDate(now, 'dd.LL.yyyy hh:mm:ss')}]: Кампания успешно запущена!`);
+    process.exit(0);
+  } catch (err) {
+    console.log(err);
+    // console.log(err.response.status);
+    console.log(`[${formatDate(now, 'dd.LL.yyyy hh:mm:ss')}]: Запрос к API WB завершился с ошибкой`);
+  }
+};
+
+timeStart = '23:23';
+const app = async () => {
+  globalTimer = setInterval(async () => {
+    const now = new Date(); 
+    const dateStart = parseDate(`${formatDate(now, 'dd.LL.yyyy')} ${timeStart}`, 'dd.LL.yyyy HH:mm', now);
+    if (!started && now > dateStart) { //  && now - dateStart < (60 * 1000 * 60) // для запуска до 12 ночи
+      await campaignAction(campaignId, 'start');
+    }
+  }, 1000);
+};
+
+await app();
