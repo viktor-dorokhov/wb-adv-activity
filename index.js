0f8aea934d5d02d7318a60d78c61257e0efe209a
Viktor Dorokhov <superdva@yandex.ru>
2024-09-05 19:20:47 +0300

    start several campaigns

--- index.js
+++ index.js
@@ -5,44 +5,50 @@ import { format as formatDate, parse as parseDate } from 'date-fns';
 const WB_API_KEY_ADV = process.env.WB_API_KEY_ADV;
 const apiUrl = 'https://advert-api.wildberries.ru/adv/v0/';
 
-// const campaignIds = process.env.CAMPAIGN_IDS.split(',');
-// пока только одна кампания
-const campaignId = process.env.CAMPAIGN_IDS;
-// пока только одно время и только старт
+const campaigns = process.env.CAMPAIGN_IDS.split(',').map((campaignId) => ({
+  id: campaignId,
+  started: false,
+}));
+// пока только одно время для всех и только старт
 const timeStart = process.env.TIME_START;
 // const timePause = process.env.TIME_PAUSE;
-let globalTimer;
-let started = false;
 
-const campaignAction = async (id, action) => {
-  const now = new Date(); 
+const getNow = () => new Date();
+
+const log = (text) => (
+  console.log(`[${formatDate(getNow(), 'dd.LL.yyyy HH:mm:ss')}]: ${text}`)
+);
+
+const getCampaignById = (campaignId) => campaigns.find(({ id }) => id === campaignId);
+
+const campaignAction = async (campaignId, action) => {
   try {
-    const response = await axios({
+    await axios({
       method: 'get',
-      url: `${apiUrl}${action}?id=${id}`,
+      url: `${apiUrl}${action}?id=${campaignId}`,
       responseType: 'json',
       headers: { 'Authorization': WB_API_KEY_ADV },
     });
-    started = true;
-    console.log(`[${formatDate(now, 'dd.LL.yyyy HH:mm:ss')}]: Кампания успешно запущена!`);
+    getCampaignById(campaignId).started = true;
+    log(`Кампания (${campaignId}) успешно запущена!`);
   } catch (err) {
     // console.log(err);
-    console.log(`[${formatDate(now, 'dd.LL.yyyy HH:mm:ss')}]: Запрос к API WB завершился с ошибкой`);
-    console.log(`Код ответа:${err.response.status}`);
-    console.log(`Текст ошибки:${err.response.data.error}`);
+    log('Запрос к API WB завершился с ошибкой');
+    console.log(`Код ответа: ${err.response.status}`);
+    console.log(`Текст ошибки: ${err.response.data.error}`);
     // process.exit(0);
   }
 };
 
 const app = async () => {
-  console.log('Приложение изменения активности WB кампаний запущено!');
-  globalTimer = setInterval(async () => {
-    const now = new Date(); 
+  console.log(`[${formatDate(getNow(), 'dd.LL.yyyy HH:mm:ss')}]: Приложение изменения активности WB кампаний запущено!`);
+  setInterval(async () => {
+    const now = getNow(); 
     const dateStart = parseDate(`${formatDate(now, 'dd.LL.yyyy')} ${timeStart}`, 'dd.LL.yyyy HH:mm', now);
-    if (!started && now > dateStart && (now - dateStart) < (1000 * 60 * 60)) {
-      await campaignAction(campaignId, 'start');
+    if (now > dateStart && (now - dateStart) < (1000 * 60 * 60)) {
+      campaigns.filter(({ started }) => !started).forEach(async ({ id }) => await campaignAction(id, 'start'));
     }
-  }, 60* 1000);
+  }, 60 * 1000);
 };
 
 await app();
